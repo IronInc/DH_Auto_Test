0c1b46aa756acc412a8d2f95ada96771169c872d
IronInc <ironguitar@gmail.com>
2018-08-22 20:54:24 +0200

    Tested one scenario with three examples. Works as expected.

---
+++ DH_Auto_Test_Exercise/DH_Auto_Test_Exercise/Features/Home page features.feature
@@ -0,0 +1,25 @@
+﻿Feature: Home page features
+
+In order to test the DH UI Testing home page
+As a user
+I want to verify home page elements exist and work as expected
+
+
+#------------------------------------------------------------------------------------------------------
+
+@uivisible
+Scenario Outline: The title UI Testing is visible regardless of the page being visited
+
+Given I am on the DH UI Testing home website
+And the title <ui_testing> is visible
+When I click on button <button>
+Then the title <ui_testing> is visible
+
+Examples:
+
+| ui_testing   | button |
+|   site	   |  form  |
+|   site	   |  home  |
+|   site	   |  error |
+
+#------------------------------------------------------------------------------------------------------
---
+++ DH_Auto_Test_Exercise/DH_Auto_Test_Exercise/HomePageFeaturesSteps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+//Hereby the libraries needed to trigger Selenium on Chrome
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DH_Auto_Test_Exercise
+{
+
+
+    [Binding]
+    public class HomePageFeaturesSteps
+    {
+        private IWebDriver driver = new ChromeDriver(@"C:\browserdrivers\chromedriver\");
+
+        //---------------------------------------------------------------------------------------------
+
+        [Given(@"I am on the DH UI Testing home website")]
+        public void GivenIAmOnTheDHUITestingHomeWebsite()
+        {
+            driver.Url = "http://uitest.duodecadits.com";
+        }
+
+        //---------------------------------------------------------------------------------------------
+
+        [Then(@"the title (.*) is visible")]
+        [Given(@"the title (.*) is visible")]
+        public void GivenTheTitleUITestingIsVisible(String title)
+        {
+            driver.FindElement(By.Id(title));
+        }
+
+        //---------------------------------------------------------------------------------------------
+
+        [When(@"I click on button (.*)")]
+        public void WhenIClickOnButtonForm(String button)
+        { 
+           driver.FindElement(By.Id(button)).Click();
+           
+        }
+
+        //---------------------------------------------------------------------------------------------
+
+    }
+}
